81645f0a27a4e2bf603cdcb2f1ecfb9b5b3f54b5
aruns05 <arun.s2k8@gmail.com>
2018-01-24 00:40:50 +0530

    Initial commit

---
+++ NorthWindWebApi/Controllers/BaseAPIController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using System.Text;
+using Newtonsoft.Json;
+using NorthWindWebApi.Models;
+
+namespace NorthWindWebApi.Controllers
+{
+    public class BaseAPIController:ApiController
+    {
+        protected readonly NorthwindDBEntities db = new NorthwindDBEntities();
+
+        protected HttpResponseMessage ToJson(dynamic obj)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+            return response;
+        }
+    }
+}
---
+++ NorthWindWebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using NorthWindWebApi.Models; 
+
+namespace NorthWindWebApi.Controllers
+{
+    public class EmployeeController : BaseAPIController
+    {
+        private NorthwindDBEntities db = new NorthwindDBEntities();
+
+        [HttpGet]
+        public IQueryable<Employee> GetEmployees()
+        {
+            var emps = db.Employees;
+            return emps;
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(Employee))]
+        public IQueryable<Employee> GetEmployee(int? employeeid)
+        {
+
+            var employee = db.Employees.Where(c => c.EmployeeID == employeeid);
+         
+            //if (employee == null)
+            //{
+            //    hr = new HttpResponseMessage(HttpStatusCode.NotFound);
+            //    return hr;
+            //}
+
+            return employee;
+        }
+
+    }
+}
